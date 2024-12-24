b947afe46427bb03251c95a60cedc8d01762918b
Pratap2018 <pratapmridha@gmail.com>
2024-12-24 11:35:01 +0530

    fix

--- script.sh
+++ script.sh
@@ -4,6 +4,6 @@ gcloud auth login --cred-file=/data/fyre-400407-9c9f737ef3f7.json
 
 gcloud config set project fyre-400407  
 
-gcloud container clusters get-credentials hypermine-gke --region asia-south1
+gcloud container clusters get-credentials hypermine-gke-manual --region asia-south1
 
 node src/index.js
